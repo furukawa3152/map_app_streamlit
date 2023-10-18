955ec36c4cd6cb0e6141d6f43081f26bb2771896
itaruchan <furukawa3152@gmail.com>
2023-10-19 00:31:49 +0900

    住所入力追加

--- map_app.py
+++ map_app.py
@@ -1,30 +1,38 @@
 import streamlit as st
 from streamlit_folium import folium_static
 import folium
+from geopy.geocoders import Nominatim
 # ページ設定
 st.set_page_config(
     page_title="streamlit-foliumテスト",
 )
+with st.form("my_form", clear_on_submit=False):
+    address = st.text_input('住所を入力してください')
+    submitted = st.form_submit_button("地図を出力")
 
-# 地図の中心の緯度/経度、タイル、初期のズームサイズを指定します。
-m = folium.Map(
-    # 地図の中心位置の指定(今回は栃木県の県庁所在地を指定)
-    location=[33.2928452, 130.297359],
-    # タイル、アトリビュートの指定
-    tiles="OpenStreetMap",
-    # ズームを指定
-    zoom_start=10
-)
+if submitted:
+    geocoder = Nominatim(user_agent="hagakure_pgm")
+    location = geocoder.geocode(address)
+    lat,lng = (location.latitude,location.longitude)
+    # 地図の中心の緯度/経度、タイル、初期のズームサイズを指定します。
+    m = folium.Map(
+        # 地図の中心位置の指定(今回は栃木県の県庁所在地を指定)
+        location=[lat,lng],
+        # タイル、アトリビュートの指定
+        tiles="OpenStreetMap",
+        # ズームを指定
+        zoom_start=10
+    )
 
-folium.Marker(
-    # 緯度と経度を指定pip
-    location=[33.2928452, 130.297359],
-    # ツールチップの指定
-    tooltip="my_home",
-    # ポップアップの指定
-    popup=folium.Popup("yeah!!", max_width=300),
-    # アイコンの指定(アイコン、色)
-    icon=folium.Icon(icon="home",icon_color="white", color="red")
-).add_to(m)
+    folium.Marker(
+        # 緯度と経度を指定pip
+        location=[33.2928452, 130.297359],
+        # ツールチップの指定
+        tooltip="my_home",
+        # ポップアップの指定
+        popup=folium.Popup("yeah!!", max_width=300),
+        # アイコンの指定(アイコン、色)
+        icon=folium.Icon(icon="home",icon_color="white", color="red")
+    ).add_to(m)
 
-st_data = folium_static(m, width=1200, height=800)
+    st_data = folium_static(m, width=800, height=800)
